74e793c83c5eb47cbee803e79e2155e40ffa5eec
Jan Drewniok <jan.drewniok.research@gmail.com>
2025-07-07 18:27:40 +0200

    :white_check_mark: simplify tests.

--- NanoLogicUITests/NanoLogicUITests.swift
+++ NanoLogicUITests/NanoLogicUITests.swift
@@ -4,17 +4,12 @@ final class NanoLogicUITests: XCTestCase {
     
     override func setUpWithError() throws {
         continueAfterFailure = false
-        let app = XCUIApplication()
-        app.launchArguments = ["-ui-testing", "-bypassSplashScreen", "-resetAppState"]
-        app.launch()
     }
-    
-    override func tearDownWithError() throws {}
     
     @MainActor
     func testAppStarts() throws {
         let app = XCUIApplication()
-        let contentView = app.otherElements["Nanotech Toolkit Logo"]
-        XCTAssertTrue(contentView.waitForExistence(timeout: 10.0), "Main content view should appear")
+        app.launchArguments = ["-ui-testing", "-bypassSplashScreen", "-resetAppState"]
+        app.launch()
     }
 }
